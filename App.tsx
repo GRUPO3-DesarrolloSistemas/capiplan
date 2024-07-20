d1388e483d283fba057f3b738c2fdfd48a5af3d3
ReitanSora <stivenpilca@gmail.com>
2024-07-20 18:26:38 -0500

    feat: add AppContext to application
    Add context to the entire application, delete unused imports

--- App.tsx
+++ App.tsx
@@ -1,14 +1,12 @@
 import * as React from 'react';
-import { NavigationContainer } from "@react-navigation/native";
-import { View } from 'react-native'; // Importa View si decides usarlo como contenedor
-import AppNavigator from "./src/components/Salud/AppNavigator";
-import Main from './src/components/main';
-
-
+import Main from './src/screens/main';
+import {AppProvider} from "./src/context/AppContext"
 
 export default function App() {
   return (
-        <Main />
+    <AppProvider>
+      <Main />
+    </AppProvider>
   );
 }
 
