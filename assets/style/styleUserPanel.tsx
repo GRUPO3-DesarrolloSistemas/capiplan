de9a06b26c9ae7657c55b6383751d08d123efb70
ReitanSora <stivenpilca@gmail.com>
2024-07-20 18:41:41 -0500

    style: add style for UserPanel view

---
+++ assets/style/styleUserPanel.tsx
@@ -0,0 +1,35 @@
+import { StyleSheet } from "react-native";
+
+import { width, height } from '../../src/constants/constants'
+import { colors } from "../../src/themes/colors";
+
+const styles = StyleSheet.create({
+    container: {
+        height: height,
+        backgroundColor: colors.white,
+    },
+    userPanelBanner: {
+        height: '25%',
+        backgroundColor: colors.lightOrange,
+        marginTop: 32,
+        alignItems: 'center'
+    },
+    userPanelImageBack: {
+        position: 'absolute',
+        bottom: -112,
+        width: 225,
+        height: 225,
+        backgroundColor: colors.white,
+        borderRadius: 225,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    buttonView: {
+        height: '50%',
+        gap: 20,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+});
+
+export default styles;
